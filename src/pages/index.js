cbe96695a629eee44ad7af2629d69ae6c0c473b8
Benjamin Saleem Brodin <bensal23@varmdogymnasium.se>
2024-11-03 21:19:38 +0100

    my changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./home";
-import Web1 from "./Web1";
-import Web2 from "./Web2";
+import Web1 from "./web1";
+import Web2 from "./web2";
 import Web3 from "./Web3";
 
 export { Home, Web1, Web2, Web3 }
