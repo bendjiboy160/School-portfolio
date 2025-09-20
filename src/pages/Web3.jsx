d1fcc57e6d95eae0e8b1fd5de7467853070954ae
bendjiboy160 <benjaminbrodin07@gmail.com>
2025-09-20 15:42:08 +0200

    web development course",

--- src/pages/Web3.jsx
+++ src/pages/Web3.jsx
@@ -14,6 +14,10 @@ const Web3 = () => {
       <motion.div variants={textVariant()} >
         <p className={styles.sectionSubText}>My Index</p>
         <h2 className={styles.sectionHeadText}>Index.</h2>
+
+      </motion.div>
+      <motion.div variants={fadeIn("","",0.1,1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        This is the index page I made for my webb server programering course. <br/>
         <a href="https://labb.vgy.se/~bensal23/webbServerProgrammering/index.php">Index</a>
       </motion.div>
     </div>
